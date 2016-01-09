6109fd5ef23d0a3eda32caccde7910706807d81d
Taiki <tyoshikawa1106@gmail.com>
2016-01-09 18:22:03 +0900

    commit

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var port = process.env.PORT || 8080;
 // Serve static files
 app.use(express.static(__dirname + '/public'));
 
+//ルーティング設定
+app.get('/thanks', function (req, res) {
+  res.send('Thank you!');
+});
+
 // Serve your app
 console.log('Served: http://localhost:' + port);
 app.listen(port);
